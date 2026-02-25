55b414da2c468cb3fda5d998ae84d0c4e8cc0111
Rohit Kumar <rohit.kumar@thesqua.re>
2026-02-25 15:14:03 +0530

    added new url for resources and availability

--- components/ReservationChart/ReservationChart.tsx
+++ components/ReservationChart/ReservationChart.tsx
@@ -110,10 +110,10 @@ const ReservationChart = ()=>{
       try {
         const endDate = dayjs(startDate).add(daysToShow, 'day').format('YYYY-MM-DD')
         // const resourcesUrl = `https://aperfectstay.ai/api/aps-pms/apts/?user=6552614495846400&start=${startDate}`
-        const resourcesUrl = `https://aperfectstay.ai/api/aps-pms/apts/?user=6552614495846400&start=${startDate}`
+        const resourcesUrl = `https://aperfectstay.ai/api/aps-pms/apts/private?start=${startDate}`
 
         const bookingsUrl = `https://aperfectstay.ai/api/aps-pms/reservations/private?start=${startDate}&end=${endDate}`
-        const availabilityUrl = `https://aperfectstay.ai/api/aps-pms/buildings/avail?user=6552614495846400&start=${startDate}&end=${endDate}`
+        const availabilityUrl = `https://aperfectstay.ai/api/aps-pms/buildings/avail/private?start=${startDate}&end=${endDate}`
         const resourcesRequest = fetch(resourcesUrl,{
           next: { revalidate: 600 } // revalidate every 60 seconds
         })
