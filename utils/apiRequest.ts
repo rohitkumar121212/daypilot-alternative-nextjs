e4f9cd3f9b1ca216748eff2836f8a9b91fb41283
Rohit Kumar <rohit.kumar@thesqua.re>
2026-02-26 11:48:57 +0530

    fixed apiRequest

--- utils/apiRequest.ts
+++ utils/apiRequest.ts
@@ -1,16 +1,21 @@
 // Development token - update this when needed
-const DEV_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NTUyNjE0NDk1ODQ2NDAwLCJlbWFpbCI6ImFwc3RyaWFsQGdtYWlsLmNvbSIsInVzZXJfdHlwZSI6MSwiZXhwIjoxNzcyMTEzMDk3LCJpYXQiOjE3NzIwODQyOTd9.CgrbT_T6_DUDRdpqXUYqGMpyJMrvndjVncUHOp21GKw'
+const DEV_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NTUyNjE0NDk1ODQ2NDAwLCJlbWFpbCI6ImFwc3RyaWFsQGdtYWlsLmNvbSIsInVzZXJfdHlwZSI6MSwiZXhwIjoxNzcyMTE1NDIwLCJpYXQiOjE3NzIwODY2MjB9.xJ8tMRrSagNf5S2eTJITa3tvCbHT94bC2t0CUCKEc3w'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 export async function apiFetch(url: string, options: RequestInit = {}) {
-  const token =  DEV_TOKEN
-  
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
     ...options.headers,
   }
   
-  if (token) {
-    headers['Authorization'] = `Bearer ${token}`
+  // Only add token in development
+  if (isDevelopment) {
+    const token =  DEV_TOKEN
+    
+    if (token) {
+      headers['Authorization'] = `Bearer ${token}`
+    }
   }
 
   const response = await fetch(url, {
