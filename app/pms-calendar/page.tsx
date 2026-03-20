8948ecd9dbfc6d2493a06ae53c576c6270eb268c
Rohit Kumar <rohit.kumar@thesqua.re>
2026-03-20 18:43:45 +0530

    before trying another approach

--- app/pms-calendar/page.tsx
+++ app/pms-calendar/page.tsx
@@ -2,7 +2,7 @@ import ReservationChart from "@/components/ReservationChart/ReservationChart";
 
 const PMS = () => {
     return (
-        <div className="h-[700px] w-[95%] mx-auto">
+        <div className="h-[600px] w-[95%] mx-auto border border-gray-300 rounded-lg shadow-md mt-2">
             <ReservationChart />
         </div>
     )
